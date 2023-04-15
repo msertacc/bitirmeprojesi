fcb9e996333302b5f07371c048e4cb2b5f182419
Ahmet Samet Şahin <ahmetsametsahin.1@gmail.com>
2023-04-15 22:40:30 +0300

    düzenlemeler

---
+++ Web/AbstractionService/Student/IStudentService.cs
@@ -0,0 +1,17 @@
+﻿using Entity.Dto.Student;
+
+namespace Abstraction.Service.Student
+{
+	public interface IStudentService
+	{
+		List<StudentDto> GetStudents();
+
+		StudentDto GetStudentById(int id);
+
+		Task Create(StudentDto studentDto);
+
+		Task Update(StudentDto studentDto);
+
+		Task Delete(int id);
+	}
+}
